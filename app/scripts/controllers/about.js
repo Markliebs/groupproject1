8de1223613316ca3d94befb721a2e812802ff8ec
markliebs <markliebs@me.com>
2016-05-23 18:24:54 -0400

    Removed uneeded folder and added favicon

--- app/scripts/controllers/about.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('App')
-  .controller('AboutCtrl', function ($scope) {
-
-  });
--- app/scripts/controllers/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-
-angular.module('App')
-  .controller('MainCtrl', function ($scope) {
-
-  });
