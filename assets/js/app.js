5db5ffb0d4be2dbec8b7cf50aedc3f29a59330d5
markliebs <markliebs@me.com>
2016-05-23 18:47:48 -0400

    adding firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,38 @@
-// $(".submitSearch").click(function () {
-// 		$('.help').modal('toggle');
+var countData = new Firebase("https://trainschedhw.firebaseio.com/");
 
-// 	});
+// Button to grab information
+$("#submitSearch").on("click", function () {
 
+    // Grabs user input
+	var newOrigin = $("#orgCount").val().trim();
+	var newDest = $("#destCount").val().trim();
+    console.log(newOrigin);
+    console.log(newDest);
+    
+    
+    
+    	var newCount = {
+		org: newOrigin,
+		dest: newDest
+    }
+
+    // Uploads employee data to the database
+	countData.push(newCount);
+
+	// Logs everything to console
+	console.log(newOrigin.org);
+	console.log(newOrigin.dest)
+
+	// Alert
+	alert("Train successfully added!");
+
+	// Clears all of the text-boxes
+	$("#orgCount").val("");
+	$("#destCount").val("");
+
+    return false;
+
+});
 	$(document).ready(function(){
     $("#helpButton").click(function(){
         $("#help").modal('toggle');
