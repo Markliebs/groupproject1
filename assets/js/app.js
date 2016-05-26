6740857c524ff0ab979e2e90d7f47dddb9658279
markliebs <markliebs@me.com>
2016-05-25 21:03:41 -0400

    merging

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,21 +5,28 @@ $("#submitSearch").on("click", function () {
 
     // Grabs user input
 	var newOrigin = $("#orgCount").val().trim();
-	var newOrigin2 = $( "#orgCount option:selected" ).text();
+	var newOrigin2 = $("#orgCount option:selected").text();
 	var newDest2 = $("#destCount").val().trim();
-	var newDest = $( "#destCount option:selected" ).text();
+	var newDest = $("#destCount option:selected").text();
+	var queryURLBase = "http://api.fixer.io/latest?base=" + newOrigin;
     console.log(newOrigin);
 	console.log(newOrigin2);
     console.log(newDest);
 	console.log(newDest2);
+	console.log(queryURLBase);
     $('#glider').html("<h2>Flying High In " + newDest + "</h2>");
- 
-    	var newCount = {
+
+	var newCount = {
 		org: newOrigin,
 		org2: newOrigin2,
 		dest: newDest,
 		dest2: newDest2
     }
+	$.ajax({ url: queryURLBase, method: "GET" })
+		.done(function (moneyData) {
+			console.log(moneyData.rates[newDest2]);
+
+		})
 
     // Uploads country data to the database
 	countData.push(newCount);
@@ -41,8 +48,8 @@ $("#submitSearch").on("click", function () {
 
 
 
-	$(document).ready(function(){
-    $("#helpButton").click(function(){
+$(document).ready(function () {
+    $("#helpButton").click(function () {
         $("#help").modal('toggle');
     });
 });
